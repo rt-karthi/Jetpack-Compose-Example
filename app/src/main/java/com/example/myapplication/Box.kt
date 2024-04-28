fd15ff8039ce25be124cc81b37629bc7440ca1bc
rt-karthi <rtkarthi27@gmail.com>
2024-04-28 11:13:07 +0530

    Box

---
+++ app/src/main/java/com/example/myapplication/Box.kt
@@ -0,0 +1,65 @@
+package com.example.myapplication
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class Box : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Box(modifier = Modifier
+            .background(Color.Red)
+        ) {
+            Box(modifier = Modifier
+                .height(60.dp)
+                .width(150.dp)
+                .background(Color.Green))
+            Text(text = "Android Developer", fontSize = 40.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoxPreview() {
+    MyApplicationTheme {
+        Greeting()
+    }
+}
